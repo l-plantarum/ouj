76c0c3e75977b490d6abfc48ea59d294de389a79
l-plantarum <katsumi@DESKTOP-5UJD8Q1.localdomain>
2019-11-04 20:18:19 +0900

    ストップワードの追加など

--- mecab.py
+++ mecab.py
@@ -15,37 +15,58 @@ tagger = MeCab.Tagger()
 client = MongoClient('mongodb://localhost:27017/')
 db = client.local
 
+i = 0
+ZEN = "".join(chr(0xff01 + i) for i in range(94))
+HAN = "".join(chr(0x21 + i) for i in range(94))
+convtbl = str.maketrans(ZEN, HAN)
 # とりあえずちょっとだけとってみる
-for qa in db.qa.find():
+for qa in db.qa20191103.find():
     lines = qa['body'].split()
     results = []
     for line in lines:
         if (reurl.match(line)):
             continue
-        wordinfo = tagger.parse(line)
+
+        # 全角→半角
+        wordinfo = tagger.parse(line.translate(convtbl))
+
+        # 〇判定
+        wordinfo = re.sub('([A-Ea-e])<>判定', '\\1判定', wordinfo)
+        # 高〇
+        wordinfo = re.sub("高校?[1一](年生?)?",  "高1", wordinfo)
+        wordinfo = re.sub("高校?[2二](年生?)?",  "高2", wordinfo)
+        wordinfo = re.sub("高校?[3三](年生?)?",  "高3", wordinfo)
 
         doc = wordinfo.split('\n')
-        for d in doc:
+        for i  in range(len(doc)):
+            d = doc[i]
+            i = i + 1
+        # for d in doc:
             if (d in ['','EOS', '/', '[', ']', '(', ')', '「', '」']):
                 continue
             dic = d.split('\t')
             if (dic[0] in [ '/', '[', ']', '(', ')', '「', '」']):
                 continue
             defs = dic[1].split(',')
-            if (defs[0] in ["連体詞", "副詞", "接続詞", "助詞", "助動詞", "記号", "数", "BOS/EOS"]):
+            if (defs[0] in ["連体詞", "副詞", "接続詞", "助詞", "助動詞", "記号", "BOS/EOS"]):
                 continue
-            if (defs[1] in ["非自立", "数"]):
+            if (defs[1] in ["非自立"]):
                 continue
             if (defs[6] in ["ない", "する", "やる", "なる", "できる", "れる"]):
+                continue
+            if (defs[6] in ["私", "志望", "高校", "志望", "合格"]):
                 continue
             if (defs[6] == '*'):
                 results.append(dic[0])
             else:
                 results.append(defs[6])
+
+    # 数値等
+
     data = {
             '_id': qa['_id'],
             'url': qa['url'],
             'postdate': qa['postdate'],
             'words': results
     }
-    db.test20190505a.insert_one(data)
+    db.test20191104.insert_one(data)
